04942eadddb6edc96dde3c1f2bad08621fd3b729
Ahmad Alfy <ahmadalfy@gmail.com>
2019-09-30 20:18:15 +0200

    feat: add babel and build scripts

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+module.exports = (api) => {
+	api.cache(true);
+
+	return {
+		presets: [
+			[
+				'@babel/env',
+				{
+					targets: {
+						browsers: 'Last 2 Chrome versions, Firefox ESR',
+						node: '8.9',
+					},
+				},
+			],
+		],
+		env: {
+			build: {
+				ignore: [
+					'**/*.test.tsx',
+					'**/*.test.ts',
+					'**/*.story.tsx',
+					'__snapshots__',
+					'__tests__',
+					'__stories__',
+				],
+			},
+		},
+		ignore: ['node_modules'],
+	};
+};

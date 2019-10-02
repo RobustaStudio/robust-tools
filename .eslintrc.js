c43ef3181a2a072dfa1d97cce718e7153a43863d
Amr Abdelkader <amrmak@gmail.com>
2019-10-02 16:47:54 +0200

    Add husky for hooks (#11)
    * chore: add husky hooks to lint commits and staged files
    
    also added node in eslintrc env to support node module.exports in commitlint config file
    
    ISSUES CLOSED: #1
    
    * chore: move husky and lintstaged configs to a separate file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,25 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es6': true,
-		'jasmine': true
+	env: {
+		browser: true,
+		es6: true,
+		jasmine: true,
+		node: true,
 	},
-	'extends': [
-		'eslint:recommended',
-		'prettier'
-	],
-	'plugins': [
-		'prettier'
-	],
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
+	extends: ['eslint:recommended', 'prettier'],
+	plugins: ['prettier'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
 	},
-	'parserOptions': {
-		'ecmaVersion': 2018,
-		'sourceType': 'module'
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
 	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'prettier/prettier': 2
-	}
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'prettier/prettier': 2,
+	},
 };

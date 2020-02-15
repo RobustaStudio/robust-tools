0b3e7adcae9a2c4004d033e51577d32a8157cce1
Ahmad Alfy <ahmadalfy@gmail.com>
2020-02-15 22:47:25 +0200

    build: run a new build

--- packages/fader/dist/fader.js
+++ packages/fader/dist/fader.js
@@ -1,122 +1,219 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+	value: true,
 });
 exports.default = void 0;
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+require('core-js/modules/es6.string.iterator');
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+require('core-js/modules/es6.array.from');
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+require('core-js/modules/es6.regexp.to-string');
 
-var Fader = function () {
-  function Fader(selector, options) {
-    _classCallCheck(this, Fader);
+require('core-js/modules/es6.object.to-string');
 
-    this.options = options;
-    this.selector = selector;
-    this.initialize();
-  }
+require('core-js/modules/es7.symbol.async-iterator');
 
-  _createClass(Fader, [{
-    key: "initialize",
-    value: function initialize() {
-      this.slider = document.querySelector(this.selector);
+require('core-js/modules/es6.symbol');
 
-      if (!this.slider) {
-        console.warn("Error: Cannot find element with the selector ".concat(this.selector));
-        return false;
-      }
+require('core-js/modules/web.dom.iterable');
 
-      this.wrapper = this.generateWrapper();
-      this.bullets = this.generateBullets();
-      this.bullets[0].click();
-    }
-  }, {
-    key: "generateWrapper",
-    value: function generateWrapper() {
-      var sliderWrapper = document.createElement('div');
-      sliderWrapper.classList.add('slider');
-      this.slider.classList.add('slider__slides');
-      this.slider.parentNode.appendChild(sliderWrapper);
-      sliderWrapper.appendChild(this.slider);
-      return sliderWrapper;
-    }
-  }, {
-    key: "generateBullets",
-    value: function generateBullets() {
-      var bulletsContainer = document.createElement('ul');
-      bulletsContainer.classList.add('slider__navigation');
-      this.slides = this.slider.children;
+require('core-js/modules/es6.object.assign');
 
-      for (var i = 0; i < this.slides.length; i++) {
-        var bullet = document.createElement('li');
-        var button = document.createElement('button');
-        button.innerHTML = i + 1;
-        button.type = 'button';
-        button.dataset.index = i;
-        button.addEventListener('click', this.handleClickedBullet.bind(this));
-        bullet.appendChild(button);
-        bulletsContainer.appendChild(bullet);
-      }
+function _toConsumableArray(arr) {
+	return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread();
+}
 
-      this.slider.parentNode.insertBefore(bulletsContainer, this.slider.nextSibling);
-      return bulletsContainer.querySelectorAll('button');
-    }
-  }, {
-    key: "handleClickedBullet",
-    value: function handleClickedBullet(ev) {
-      var active = ev.target.closest('ul').querySelector('.active');
+function _nonIterableSpread() {
+	throw new TypeError('Invalid attempt to spread non-iterable instance');
+}
 
-      if (active) {
-        active.classList.remove('active');
-      }
+function _iterableToArray(iter) {
+	if (
+		Symbol.iterator in Object(iter) ||
+		Object.prototype.toString.call(iter) === '[object Arguments]'
+	)
+		return Array.from(iter);
+}
 
-      ev.target.classList.add('active');
-      var index = ev.target.dataset.index;
-      this.resetActiveSlide();
-      this.setActiveSlide(index);
+function _arrayWithoutHoles(arr) {
+	if (Array.isArray(arr)) {
+		for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) {
+			arr2[i] = arr[i];
+		}
+		return arr2;
+	}
+}
 
-      if (this.timer) {
-        clearTimeout(this.timer);
-      }
+function _classCallCheck(instance, Constructor) {
+	if (!(instance instanceof Constructor)) {
+		throw new TypeError('Cannot call a class as a function');
+	}
+}
 
-      this.startTimer(+index);
-    }
-  }, {
-    key: "resetActiveSlide",
-    value: function resetActiveSlide() {
-      var active = this.wrapper.querySelector('.slider__slide--active');
+function _defineProperties(target, props) {
+	for (var i = 0; i < props.length; i++) {
+		var descriptor = props[i];
+		descriptor.enumerable = descriptor.enumerable || false;
+		descriptor.configurable = true;
+		if ('value' in descriptor) descriptor.writable = true;
+		Object.defineProperty(target, descriptor.key, descriptor);
+	}
+}
 
-      if (active) {
-        active.classList.remove('slider__slide--active');
-      }
-    }
-  }, {
-    key: "setActiveSlide",
-    value: function setActiveSlide(index) {
-      this.slides[index].classList.add('slider__slide--active');
-    }
-  }, {
-    key: "startTimer",
-    value: function startTimer(index) {
-      var _this = this;
+function _createClass(Constructor, protoProps, staticProps) {
+	if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+	if (staticProps) _defineProperties(Constructor, staticProps);
+	return Constructor;
+}
 
-      var nextSlideIndex = index + 1;
-      this.timer = setTimeout(function () {
-        if (!_this.slides[nextSlideIndex]) {
-          nextSlideIndex = 0;
-        }
+var Fader = (function() {
+	function Fader(selector, options) {
+		_classCallCheck(this, Fader);
 
-        _this.bullets[nextSlideIndex].click();
-      }, 5000);
-    }
-  }]);
+		this.options = options;
+		var defaultOptions = {
+			wrapperClasses: ['slider'],
+			titleAnimation: '',
+			transition: 5000,
+		};
+		Object.assign(defaultOptions, this.options);
+		this.selector = selector;
+		this.initialize();
+	}
 
-  return Fader;
-}();
+	_createClass(Fader, [
+		{
+			key: 'initialize',
+			value: function initialize() {
+				this.slider = document.querySelector(this.selector);
+
+				if (!this.slider) {
+					console.warn('Error: Cannot find element with the selector '.concat(this.selector));
+					return false;
+				}
+
+				this.wrapper = this.generateWrapper();
+				this.bullets = this.generateBullets();
+
+				if (this.options.titleAnimation) {
+					this.assignAnimation();
+				}
+
+				this.bullets[0].click();
+			},
+		},
+		{
+			key: 'assignAnimation',
+			value: function assignAnimation() {
+				var _this = this;
+
+				var titles = this.wrapper.querySelectorAll('.slider__title');
+
+				if (titles.length > 0) {
+					titles.forEach(function(title) {
+						title.classList.add(_this.options.titleAnimation);
+					});
+				}
+			},
+		},
+		{
+			key: 'generateWrapper',
+			value: function generateWrapper() {
+				var _sliderWrapper$classL;
+
+				var sliderWrapper = document.createElement('div');
+
+				(_sliderWrapper$classL = sliderWrapper.classList).add.apply(
+					_sliderWrapper$classL,
+					_toConsumableArray(this.options.wrapperClasses),
+				);
+
+				this.slider.classList.add('slider__slides');
+				this.slider.parentNode.appendChild(sliderWrapper);
+				sliderWrapper.appendChild(this.slider);
+				return sliderWrapper;
+			},
+		},
+		{
+			key: 'generateBullets',
+			value: function generateBullets() {
+				var bulletsContainer = document.createElement('ul');
+				bulletsContainer.classList.add('slider__navigation');
+				this.slides = this.slider.children;
+
+				for (var i = 0; i < this.slides.length; i++) {
+					var bullet = document.createElement('li');
+					var button = document.createElement('button');
+					button.innerHTML = i + 1;
+					button.type = 'button';
+					button.dataset.index = i;
+					button.addEventListener('click', this.handleClickedBullet.bind(this));
+					bullet.appendChild(button);
+					bulletsContainer.appendChild(bullet);
+				}
+
+				this.slider.parentNode.insertBefore(bulletsContainer, this.slider.nextSibling);
+				return bulletsContainer.querySelectorAll('button');
+			},
+		},
+		{
+			key: 'handleClickedBullet',
+			value: function handleClickedBullet(ev) {
+				var active = ev.target.closest('ul').querySelector('.active');
+
+				if (active) {
+					active.classList.remove('active');
+				}
+
+				ev.target.classList.add('active');
+				var index = ev.target.dataset.index;
+				this.resetActiveSlide();
+				this.setActiveSlide(index);
+
+				if (this.timer) {
+					clearTimeout(this.timer);
+				}
+
+				this.startTimer(+index);
+			},
+		},
+		{
+			key: 'resetActiveSlide',
+			value: function resetActiveSlide() {
+				var active = this.wrapper.querySelector('.slider__slide--active');
+
+				if (active) {
+					active.classList.remove('slider__slide--active');
+				}
+			},
+		},
+		{
+			key: 'setActiveSlide',
+			value: function setActiveSlide(index) {
+				this.slides[index].classList.add('slider__slide--active');
+			},
+		},
+		{
+			key: 'startTimer',
+			value: function startTimer(index) {
+				var _this2 = this;
+
+				var nextSlideIndex = index + 1;
+				this.timer = setTimeout(function() {
+					if (!_this2.slides[nextSlideIndex]) {
+						nextSlideIndex = 0;
+					}
+
+					_this2.bullets[nextSlideIndex].click();
+				}, this.options.transition);
+			},
+		},
+	]);
+
+	return Fader;
+})();
 
 var _default = Fader;
 exports.default = _default;
